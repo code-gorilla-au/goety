39558cc5a12192d8cc6bb179d3f18942bb4b9abe
frag223 <m.riquelme223@gmail.com>
2025-07-18 22:13:36 +1000

    fixing

--- scripts/tools.mk
+++ scripts/tools.mk
@@ -6,7 +6,6 @@ tools-all: tools-dev tools-scan ## Get all tools for development
 
 tools-scan: ## get all the tools required
 	go install golang.org/x/vuln/cmd/govulncheck@latest
-	go install github.com/securego/gosec/v2/cmd/gosec@latest
 	go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 
 
